4716c40fea158cde0674192f5a0c19b04553588b
Emmanuex <olaworeemmanuex@gmail.com>
2022-09-16 15:53:25 -0700

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,42 @@
+
+ehoneahobed
+/
+alx-low_level_programming
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+alx-low_level_programming/0x03-debugging/1-main.c
+@ehoneahobed
+ehoneahobed Update task 1
+ 1 contributor
+24 lines (17 sloc)  263 Bytes
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*)*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
+
